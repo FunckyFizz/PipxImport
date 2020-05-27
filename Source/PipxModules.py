46b52fb4e5b3e66b4912a977c6f8fc6610d2fc4c
FunckyFizz <62399911+FunckyFizz@users.noreply.github.com>
2020-05-27 15:08:41 +0100

    Windows Support Added
    At the moment this will only work for OpenCV. A future version will be to look for a "pipxed" module more dynamically.

--- Source/PipxModules.py
+++ Source/PipxModules.py
@@ -1,15 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 def PipxImport(mod):
     import os
     
     CL = os.getcwd() 
     
-    os.chdir(os.path.expanduser("~") + '/.local/pipx/venvs/opencv-python/lib64/python3.8/site-packages') 
+    if os.name == 'nt':
+        os.chdir(os.path.expanduser("~") + r'\.local\pipx\venvs\opencv-python\Lib\site-packages') 
+    else:
+        os.chdir(os.path.expanduser("~") + '/.local/pipx/venvs/opencv-python/lib64/python3.8/site-packages') 
     
     mod = __import__(mod)
     
