cbda4bfc7edb2e246d84d3ead3cad68179886ae8
FunckyFizz <62399911+FunckyFizz@users.noreply.github.com>
2020-05-28 16:54:30 +0100

    Added dynamic module importing
    You can now dynamically import any pipxed module. If the module your importing has a different import name to its install name, simply give the install name, also as a string, after the import name.

--- Source/PipxModules.py
+++ Source/PipxModules.py
@@ -1,15 +1,25 @@
-def PipxImport(mod):
+def PipxImport(ImportName,*InstallName):
     import os
     
-    CL = os.getcwd() 
+    CL = os.getcwd()
+    
+    FolderName = ImportName
+    
+    i = 0    
+    for arg in InstallName:
+        if i == 0:
+            FolderName = arg
+        else:
+            break
+        i = i + 1
     
     if os.name == 'nt':
-        os.chdir(os.path.expanduser("~") + r'\.local\pipx\venvs\opencv-python\Lib\site-packages') 
+        os.chdir(os.path.expanduser("~") + '\\.local\\pipx\\venvs\\' + FolderName + '\\Lib\\site-packages') 
     else:
-        os.chdir(os.path.expanduser("~") + '/.local/pipx/venvs/opencv-python/lib64/python3.8/site-packages') 
+        os.chdir(os.path.expanduser("~") + '/.local/pipx/venvs/' + FolderName + '/lib64/python3.8/site-packages') 
     
-    mod = __import__(mod)
+    ImportName = __import__(ImportName)
     
     os.chdir(CL)
     
-    return mod
+    return ImportName
